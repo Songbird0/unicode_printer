b869af5fde29f0b027689d294092051fc1c941cc
Anthony Defranceschi <chaacygg@gmail.com>
2017-09-07 03:43:10 +0200

    some stuff

---
+++ latin-1_accent.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+code_point_threshold = 0x00FF
+current_char_code_point = (code_point_threshold - 0x0080) + 0x0022  # = 161
+
+rule_content = "{ "
+while current_char_code_point < 0x00FF:
+    current_char = chr(current_char_code_point)
+    rule_content += ("\"" + current_char + "\"")
+    rule_content += " | "
+    current_char_code_point += 0x01
+rule_content += ("\"" + chr(current_char_code_point) + "\"")
+rule_content += " }"
+
+rule_name = "accents"
+rule = rule_name + " = " + rule_content
+
+with open('markdown.pest', 'a', encoding='utf-8') as pest_file:
+    pest_file.write('{0}\n'.format(rule))
---
+++ punctuation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+code_point_threshold = 0x007F
+
+current_char_code_point = 0x0021
+
+rule_content = "{ "
+while current_char_code_point < code_point_threshold:
+    current_char = chr(current_char_code_point)
+    if not current_char.isalpha() and not current_char.isdigit() and not current_char.isspace():
+        rule_content += ("\"" + current_char + "\"")
+        rule_content += " | "
+    print("character={0}\ncode point={1}\n----------".format(current_char, current_char_code_point))
+    current_char_code_point += 0x1
+rule_content += ("\"" + chr(current_char_code_point) + "\"")
+rule_content += " }"
+
+rule_name = "punctuation"
+rule = rule_name + " = " + rule_content
+
+with open('markdown.pest', 'a', encoding='utf-8') as pest_file:
+    pest_file.write('{0}'.format(rule))
